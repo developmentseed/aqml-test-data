868ed4f0065fd411d643df21295b5b3fc82acc92
sethvincent <sethvincent@gmail.com>
2018-05-23 12:53:25 -0700

    fix mistakes

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ module.exports = {
   randomPointsInPolygon: require('./lib/random-points-in-polygon'),
   data: {
     houston: {
-      boundaries: require('./data/houston-boundaries'),
-      pm25: require('./data/houston-pm25')
+      boundaries: require('./data/houston-boundaries.json'),
+      pm25: require('./data/houston-pm25.json')
     }
   }
 }
